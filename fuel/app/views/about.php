78b36e9dfc389da2f2e6df01c04ffc6b21a6d6c5
Jared-Kirschbaum <65467962+Jared-Kirschbaum@users.noreply.github.com>
2023-04-01 09:46:21 -0700

    Add files via upload

---
+++ fuel/app/views/about.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>About</title>
+    <link rel="stylesheet" type="text/css" href="about.css">
+  </head>
+  <body>
+     </head>
+  <body>
+    <div class="navbar">
+      <i href="home.html"><img src="C:\Users\jared\Documents\CS312\m1\logo.png" scale=></img></i>
+      <a href="home.html">Home</a>
+      <a class = "active" href="about.html">About</a>
+      <a href="about.css">Color Coordinate Generation</a>
+    </div>
+    <p></p>
+    <h1>Company Leadership</h1>
+    <h3>Jared Kirschbaum | Founder / Co-Owner</h3>
+    <div class="container">
+        <img src="C:\Users\jared\OneDrive\Pictures\jared.png" alt="Person 1" />
+      <p>With a background in data science and a strong passion to share knowledge
+         and lead by example through technical expertise. Jared is highly capable of working alone or as a member of a team, and 
+         when the need arises to take the initiative by solving the most challenging of problems to ensure customer satisfaction and product enhancement.
+         With over 9+ years in the intelligence community and special operations, he aims to integrate leading Artificial Intelligence
+         practices into the defense and national security industry.</p>
+    </div>
+    <h3>Edelen Hunter | Founder / Co-Owner</h3>
+    <div class="container">
+        <img src="C:\Users\jared\Documents\CS312\m1\logo.png" alt="Person 2" />
+      <p>
+        Edelen Hunter
+      </p>
+    </div>
+    <h3>Stransky Justin | Founder / Co-Owner</h3>
+    <div class="container">
+        <img src="C:\Users\jared\Documents\CS312\m1\logo.png" alt="Person 3" />
+      <p>
+        Stransky Justin
+      </p>
+    </div>
+  </body>
+</html>
---
+++ fuel/app/views/home.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>M1</title>
+    <link rel="stylesheet" type="text/css" href="home.css">
+  </head>
+  <body>
+     </head>
+  <body>
+    <div class="navbar">
+      <i href="home.html"><img src="C:\Users\jared\Documents\CS312\m1\logo.png" scale=></img></i>
+      <a class = "active" href="home.html">Home</a>
+      <a href="about.html">About</a>
+      <a href="home.css">Color Coordinate Generation</a>
+    </div>
+    <div class="container">
+        <div class = "container_two">
+        <h1>ESK</h1>
+        </div>
+        <div class = "container_two">
+        <h3>Collaborate on Buisness Needs</h3>
+        <p>ESK is a leading technology company that specializes in artificial intelligence,
+            front-end development using Fuel, PHP, HTML, and CSS, and backend development using MySQL and Python.
+            With a team of experienced professionals, ESK is dedicated to providing innovative solutions that help businesses
+            stay ahead of the curve in today's rapidly evolving digital landscape.</p>
+        </div>
+
+        <div class = "container_two">
+        <h3>Leverage AI</h3>
+        <p>At ESK, we believe that artificial intelligence is the future of technology, and we are committed to leveraging
+           this powerful technology to help our clients solve complex business challenges. Our AI solutions are tailored to
+           meet the unique needs of each client, and we work closely with them to ensure that our solutions are effective and efficient.</p>
+        </div>
+
+        <div class = "container_two">
+        <h3>Front End Development</h3>
+        <p>In addition to AI, we specialize in front-end development using Fuel, PHP, HTML, and CSS. Our team of designers and developers
+           are passionate about creating user-friendly and visually appealing websites and applications that deliver an exceptional user 
+           experience.</p>
+        </div>
+        
+        <div class = "container_two">
+        <h3>Backend Development</h3>
+        <p>On the backend side, we utilize the power of MySQL and Python to develop robust and scalable applications that can handle large amounts
+           of data and complex workflows. Our backend solutions are designed to be secure, efficient, and easy to maintain, ensuring that our clients 
+           can focus on growing their businesses without worrying about the technical details.</p>
+        </div>
+        
+        <div class = "container_two">
+        <h3>Vision Statement</h3>
+        <p>At ESK, we are dedicated to providing our clients with the highest level of service and support. We work closely with them to understand their
+           unique business needs and goals, and we are committed to delivering solutions that exceed their expectations. If you are looking for a 
+           technology partner that can help you stay ahead of the curve, look no further than ESK.</p>        
+        </div>
+    </div>
+  </body>
+</html>
