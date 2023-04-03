d724e2038e5d351820b5f0619cf74562b09f4a33
Justin Stransky <dva@colostate.edu>
2023-04-03 16:26:27 -0400

    Complete CCG Success of Fail functionality

--- fuel/app/views/generator/index.php
+++ fuel/app/views/generator/index.php
@@ -4,7 +4,23 @@
     <meta charset="UTF-8">
     <title>CCG</title>
     <meta name="description" content="Color Coordinate Generator">
-    <!-- <link href="./style.css" type="text/css" rel="stylesheet"> -->
+    <style>
+      .error {
+        color: #EA4AAA;
+        background-color: #FCEDF5;
+        font-family: 'system-ui';
+        padding: 5px;
+      }
+      table,
+      td {
+          border: 1px solid #333;
+      }
+      thead,
+      tfoot {
+          background-color: #333;
+          color: #fff;
+      }
+    </style>
   </head>
 
   <header>
@@ -16,37 +32,45 @@
   <body>
     <div>
       <form method="get" action="generator.php">
-        <label for="size">Number of rows/columns (one value)?</label>
-        <input type="number" name="size" id="size">
-    
+        <label for="size">Number of rows and columns?</label>
+        <input type="number" name="size" id="size" value=<?php echo $size;?> onClick="this.select();">
+        <?php
+        if ($valid_size == false) {
+          $size_error_placeholder = 'error';
+        }
+        ?>
+        <span class=<?php echo $size_error_placeholder ?>><?php echo $size_error_message;?></span>
+        <br><br>
+
         <label for="colors">Number of colors?</label>
-        <input type="number" name="colors" id="colors">
+        <input type="number" name="colors" id="colors" value=<?php echo $colors;?> onClick="this.select();">
+        <?php
+        if ($valid_colors == false) {
+          $colors_error_placeholder = 'error';
+        }
+        ?>
+        <span class=<?php echo $colors_error_placeholder ?>><?php echo $colors_error_message;?></span>
+        <br><br>
 
         <input type="submit" value="Submit"> 
       </form>
-      <p>Size: <?php echo $size ?></p>
-      <p><?php
-      if ($valid_size) {
-        echo 'SUCCESS: ';
-        echo $size . ' is a valid size of rows and columns.';
-      }
-      else {
-        echo 'FAILURE: ';
-        echo $size . ' is an invalid size of rows and columns.';
-      }
-      ?></p>
+
       <br>
-      <p>Colors: <?php echo $colors ?></p>
-      <p><?php
-      if ($valid_colors) {
-        echo 'SUCCESS: ';
-        echo $colors . ' is a valid number of colors.';
-      }
-      else {
-        echo 'FAILURE: ';
-        echo $colors . ' is an invalid number of colors.';
-      }
-      ?></p>
+      <?php if($valid_size && $valid_colors): ?>
+        <table>
+          <thead>
+              <tr>
+                  <th colspan="2">CCG Table</th>
+              </tr>
+          </thead>
+          <tbody>
+              <tr>
+                  <td>Size: <?php echo $size ?></td>
+                  <td>Colors: <?php echo $colors ?></td>
+              </tr>
+          </tbody>
+        </table>
+      <?php endif; ?>
     </div>
   </body>
 </html>
