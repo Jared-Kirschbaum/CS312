2c72fd3379ca145be1d150ae8a0821ea6d40858e
Jared-Kirschbaum <65467962+Jared-Kirschbaum@users.noreply.github.com>
2023-04-03 13:00:39 -0700

    Update index.php

--- fuel/app/views/generator/index.php
+++ fuel/app/views/generator/index.php
@@ -1,5 +1,110 @@
 <!DOCTYPE html>
 <html>
+  <style>
+    
+        label {
+
+            font-family: "Arial";
+
+            font-size: larger;
+
+            margin-right: 20px;
+
+            color: white;
+
+        }.navbar {
+            background-color: rgb(26, 27, 24)000; 
+            background-repeat: no-repeat;
+            background-position: left;
+            background-size: contain;
+            height: 100px; 
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            padding-right: 36%;
+          }
+
+
+          .navbar a {
+            font-family: 'Arial';
+            float: left;
+            color: #f2f2f2;
+            text-align: center;
+            padding: 10px 16px;
+            text-decoration: none;
+            font-size: 20px;
+        }
+
+
+        .navbar i {
+                height: 100px;
+                width: 100px;
+                float: left;
+                padding-left: 2%;
+                margin: 0 auto;
+                padding: 10px 0;
+                padding-right: 50%;
+        }
+
+        .navbar i img {
+        object-fit: cover;
+        height: 100px;
+
+        }
+
+          .navbar a:hover {
+            background-color: #ffffff;
+            color: rgb(0, 0, 0);
+        }
+
+          .navbar a.active {
+            background-color: #ffffff;
+            color: rgb(0, 0, 0);
+        }
+
+          body {
+            background-color: rgb(26, 27, 24);
+        }
+
+          p {
+            font-family: 'Arial';
+            font-size: larger;
+            margin-right: 20px;
+            color: white;
+        }
+
+          /* Style for the container */
+          .container {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            align-items: center;
+            margin: 20px;
+        }
+
+        .container_two {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            align-items: center;
+            margin: 20px;
+          }
+
+
+        h3 {
+            font-family: "Arial";
+            color: white;
+        }
+
+        h1 {
+            font-family: "Arial";
+            font-size: 100px;
+            color: white;
+        }
+    
+  </style> 
+    
+
   <head>
     <meta charset="UTF-8">
     <title>CCG</title>
@@ -9,7 +114,7 @@
 
   <header>
     <div>
-      <h1>Color Coordinate Generator</h1>
+      <h3>Color Coordinate Generator</h3>
     </div>
   </header>
 
